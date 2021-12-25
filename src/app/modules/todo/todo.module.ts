2bd7e3bf85422da5033ed7f840b811fc152339db
fleek-moon <fleek.moon@kakaocorp.com>
2021-12-25 19:42:36 +0900

    chore : Add component in module

--- src/app/modules/todo/todo.module.ts
+++ src/app/modules/todo/todo.module.ts
@@ -5,6 +5,11 @@ import { TodoContainerComponent } from "./components/todo-container/todo-contain
 import { TodoListComponent } from "./components/todo-list/todo-list.component";
 import { TodoFormComponent } from "./components/todo-form/todo-form.component";
 import { ReactiveFormsModule } from "@angular/forms";
+import { FormChildComponent } from "./components/form-child/form-child.component";
+import { TodoItemChildComponent } from "./components/todo-item-child/todo-item-child.component";
+import { UnrelatedParentComponent } from "./components/unrelated-parent/unrelated-parent.component";
+import { UnrelatedChildComponent } from "./components/unrelated-child/unrelated-child.component";
+import { FormChildOfChildComponent } from './components/form-child-of-child/form-child-of-child.component';
 
 @NgModule({
   declarations: [
@@ -12,6 +17,11 @@ import { ReactiveFormsModule } from "@angular/forms";
     TodoContainerComponent,
     TodoListComponent,
     TodoFormComponent,
+    FormChildComponent,
+    TodoItemChildComponent,
+    UnrelatedParentComponent,
+    UnrelatedChildComponent,
+    FormChildOfChildComponent,
   ],
   imports: [CommonModule, ReactiveFormsModule],
   exports: [TodoContainerComponent],
